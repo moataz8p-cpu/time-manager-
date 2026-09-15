463e034049e821517fff1ce5a0da81562f1d80ff
moataz8p-cpu <moataz8p@gmail.com>
2026-09-15 11:17:18 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,13 +5,18 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(
+        RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    )
+
     repositories {
         google()
         mavenCentral()
     }
 }
 
-rootProject.name = "WaqtOrganizer"
+rootProject.name = "TimeManager"
+
 include(":app")
